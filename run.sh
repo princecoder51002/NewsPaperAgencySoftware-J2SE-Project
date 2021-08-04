db4c1f6087166e2b93311f61179435697af28826
princecoder51002 <wadhwacorporations51002@gmail.com>
2021-08-04 13:23:29 +0530

    adding runable file and when u want to run this jar file , run by using ./run.sh

---
+++ run.sh
@@ -0,0 +1,2 @@
+ java --module-path /home/prince/Downloads/openjfx-16_linux-x64_bin-sdk/javafx-sdk-16/lib/ --add-modules=javafx.controls,javafx.graphics,javafx.fxml,javafx.web -jar NewsPaperAgencySoftwareApp.jar 
+
